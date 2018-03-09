41607885299cfb3b5785df3483bc6f9eead24212
Klimenko Anna <milaanya@yandex.ru>
2018-03-10 00:18:55 +0300

    Make GUI(dialog)

--- core/Manager.py
+++ core/Manager.py
@@ -1,23 +1,169 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from MessengerAPI.Authorization import Authorization
+
 
 class Manager(QMainWindow):
     __authorization = Authorization.getInstance()
 
     def __init__(self):
         super().__init__()
+        self.rightSubMenu = QVBoxLayout()
         self.initUI()
-        self.authVK()
 
     def initUI(self):
-        self.statusBar().showMessage('Ready')
-        self.resize(600, 500)
+        # self.statusBar().showMessage('Ready')
+        self.resize(800, 600)
         self.setWindowTitle('Messenger')
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.setSpacing(0)
+        mainLayout.setContentsMargins(0, 0, 0, 0)
+        wid.setLayout(mainLayout)
+
+        leftMenu = QVBoxLayout()
+        self.rightMenu = QVBoxLayout()
+        self.rightMenu.setSpacing(0)
+
+        mainLayout.addLayout(leftMenu)
+        mainLayout.addLayout(self.rightMenu)
+        leftMenu.addWidget(QPushButton('Friend1btn2'))
+        leftMenu.addWidget(QPushButton('Friend2btn3'))
+        leftMenu.addItem(QSpacerItem(220, 0, QSizePolicy.Fixed, QSizePolicy.Expanding))
+        self.rightMenu.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.MessageMenuInit()
         self.show()
-        pass   
-    def authVK(self):
+        pass
+
+    def clearLayout(self,layout):
+        while layout.count():
+            child = layout.takeAt(0)
+            if child.widget() is not None:
+                child.widget().deleteLater()
+            elif child.layout() is not None:
+                self.clearLayout(child.layout())
+        self.rightMenu.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
+
+    def OpenSettings(self):
+        self.clearLayout(self.rightMenu)
+        msgInit = QPushButton('Close')
+        msgInit.clicked.connect(self.MessageMenuInit)
+        self.rightMenu.addWidget(msgInit)
+        vkLogin = QPushButton('Login throw VK')
+        vkLogin.clicked.connect(self.loginThrowVK)
+        self.rightMenu.addWidget(vkLogin)
+
+
+    def loginThrowVK(self):
         self.__authorization.authorizationVK(self)
+        pass
+
+
+    def MessageMenuInit(self):
+        self.clearLayout(self.rightMenu)
+        self.addUserSubMenu(self.rightSubMenu)
+        self.addMessageLayout(self.rightSubMenu)
+        self.MessageWriteMenu(self.rightSubMenu)
+        self.rightMenu.addLayout(self.rightSubMenu)
+
+    def MessageWriteMenu(self,layout):
+        msgEnter = QWidget()
+        msgEnter.setStyleSheet("background-color:#bef3eb;")
+        rightMessageEnter = QHBoxLayout(msgEnter)
+        rightMessageEnter.setContentsMargins(0, 0, 0, 0)
+        attachButton = QPushButton()
+        attachButton.setIcon(QIcon('../resources/attachmentIcon.png'))
+        attachButton.setIconSize(QSize(40, 40))
+        attachButton.setStyleSheet("background-color: transparent")
+        rightMessageEnter.addWidget(attachButton)
+        lineEdit = QTextEdit()
+        lineEdit.setPlainText("Enter message...")
+        lineEdit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+        lineEdit.setStyleSheet("border: 1px solid black;border-radius:5px;background-color:white;")
+        lineEdit.setMaximumSize(10000, 35)
+        rightMessageEnter.addWidget(lineEdit)
+        sctickersButton = QPushButton()
+        sctickersButton.setIcon(QIcon('../resources/smileIcon.png'))
+        sctickersButton.setIconSize(QSize(30, 30))
+        sctickersButton.setStyleSheet("background-color: transparent")
+        rightMessageEnter.addWidget(sctickersButton)
+        sendButton = QPushButton()
+        sendButton.setIcon(QIcon('../resources/sendMessageIcon.png'))
+        sendButton.setIconSize(QSize(30, 30))
+        sendButton.setStyleSheet("background-color: transparent;margin-right:15px;")
+        rightMessageEnter.addWidget(sendButton)
+        layout.addWidget(msgEnter)
+
+    def addMessageLayout(self, layout):
+        bcgColor2 = QWidget()
+        bcgColor2.setStyleSheet("background-color: white")
+        rightMessageHistory = QVBoxLayout(bcgColor2)
+        # rightMessageHistory.addItem(QSpacerItem(20, 40, QSizePolicy.Expanding, QSizePolicy.Expanding))
+        widgetList = QListWidget()
+        rightMessageHistory.setContentsMargins(0, 0, 0, 0)
+        widgetList.setStyleSheet("""QListWidget{border: 0;}
+                                QScrollBar::handle:vertical {
+                                    background: #DCDCDC;
+                                    border-radius: 10px;
+                                    min-height: 20px;
+                                }""")
+        # widgetList.setFlow(Qt.AlignBottom)
+        rightMessageHistory.addWidget(widgetList)
+        for x in range(0, 50):
+            self.addMessageToLayout("test " + str(x), widgetList, True)
+            self.addMessageToLayout("test " + str(x), widgetList, False)
+
+        layout.addWidget(bcgColor2)
+
+    '''
+        sideMessage
+            True - my message
+            False - message from friend
+    '''
+    def addMessageToLayout(self, message, layout, sideMessage):
+        helpWidget = QWidget()
+        msg = QGridLayout(helpWidget)
+        msg.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Expanding))
+
+        msgButton = QPushButton(message)
+        msgButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        msg.addWidget(msgButton, 0, 0, Qt.AlignRight)
+        if sideMessage:
+            msg.addWidget(msgButton, 0, 0, Qt.AlignRight)
+        else:
+            msg.addWidget(msgButton, 0, 0, Qt.AlignLeft)
+
+        itemN = QListWidgetItem()
+        itemN.setSizeHint(QSize(0, 50))
+        layout.addItem(itemN)
+        layout.setItemWidget(itemN, helpWidget)
+
+    def addUserSubMenu(self, layout):
+        backgroundColor = QWidget()
+        backgroundColor.setStyleSheet("background-color: #e3dcd6")
+        rightSubMenu = QHBoxLayout(backgroundColor)
+        iconProfile = QPushButton()
+        iconProfile.setIcon(QIcon('../resources/testProfileLogo.png'))
+        iconProfile.setIconSize(QSize(35, 35))
+        iconProfile.setStyleSheet("background-color: white;border-radius: 17px;")
+        rightSubMenu.addWidget(iconProfile)
+        profileBtn = QPushButton('Dude profile')
+        profileBtn.setStyleSheet("background-color: transparent;border-radius: 20px;font-size: 10pt;")
+        rightSubMenu.addWidget(profileBtn)
+        rightSubMenu.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        settingsButton = QPushButton()
+        settingsButton.clicked.connect(self.OpenSettings)
+        settingsButton.setIcon(QIcon('../resources/settingsIcon.png'))
+        settingsButton.setIconSize(QSize(30, 30))
+        settingsButton.setStyleSheet("background-color: transparent;margin-right:15px;")
+        rightSubMenu.addWidget(settingsButton)
+        layout.addWidget(backgroundColor)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
